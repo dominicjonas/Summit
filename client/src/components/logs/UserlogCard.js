01edd27b380e1a065f3bab456365c86eb8ef21f5
Dominic Jonas <ppkk.dominic@gmail.com>
2021-11-22 18:20:34 +0000

    formatted incoming date and time for userlog cards

--- client/src/components/logs/UserlogCard.js
+++ client/src/components/logs/UserlogCard.js
@@ -3,9 +3,18 @@ import { CgCloseR } from 'react-icons/cg'
 import { AiOutlineEllipsis } from 'react-icons/ai'
 
 const UserlogCard = ({ weight, sets, reps, date }) => {
+  const formattedDate = date.split('').splice(0, 10).join('')
+  const formattedTime = date.split('').splice(11, 8).join('')
+
+  // console.log(formattedDate)
+  console.log(formattedDate + formattedTime)
+
   return (
     <div className='userlog-card'>
-      <p className='date'>Session: {date}</p>
+      <p className='date'>
+        Session: <span className='card-date'>{formattedDate}</span>{' '}
+        <span className='card-time'>{formattedTime}</span>
+      </p>
       <div className='userlog-card-weight-container'>
         <p>
           SESSION PB: <span className='card-pb'>{weight}kg</span>
