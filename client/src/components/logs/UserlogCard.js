4e768cb0970065e0d7a38872385745d665441c13
Dominic Jonas <ppkk.dominic@gmail.com>
2021-11-22 14:40:57 +0000

    added userlog-card styles

--- client/src/components/logs/UserlogCard.js
+++ client/src/components/logs/UserlogCard.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import { CgCloseR } from 'react-icons/cg'
+import { AiOutlineEllipsis } from 'react-icons/ai'
 
 const UserlogCard = ({ weight, sets, reps, date }) => {
   return (
     <div className='userlog-card'>
-      <p>weight: {weight}</p>
-      <p>sets: {sets}</p>
-      <p>reps: {reps}</p>
-      <p>date: {date}</p>
-      <div>
-        <button onClick={() => removeTodo(index)}>✕</button>
+      <p className='date'>Session: {date}</p>
+      <div className='userlog-card-weight-container'>
+        <p>Session Personal Best: {weight}kg</p>
+        <AiOutlineEllipsis className='dot' />
+        <p>sets: {sets}</p>
+        <AiOutlineEllipsis className='dot' />
+        <p>reps: {reps}</p>
+      </div>
+      <div className='remove-btn' onClick={() => removeTodo(index)}>
+        <CgCloseR />
       </div>
     </div>
   )
